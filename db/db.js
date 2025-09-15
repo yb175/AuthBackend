a589ac114bf8ded82a2a34fd74c4480470d1ddcc
Yug Bhatia <bhatiayug175@gmail.com>
2025-09-15 18:57:47 +0530

    Mongo db string hidden

--- db/db.js
+++ db/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 async function connectDB() {
     try {
-        await mongoose.connect("mongodb+srv://bhatiayug175:bhatiabhai@cluster0.ic1q8bn.mongodb.net/sih");
+        await mongoose.connect(process.env.MONGODB_KEY);
         console.log("Database connected");
     } catch (error) {
         console.log("Error is ", error.message);
