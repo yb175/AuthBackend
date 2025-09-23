3cf0c64228ce6c9a4d5ca7049a3f016ba7b24904
Yug Bhatia <bhatiayug175@gmail.com>
2025-09-23 18:39:02 +0530

    Redis password hidden

--- config/redis.js
+++ config/redis.js
@@ -1,7 +1,9 @@
+import dotenv from "dotenv" ; 
+dotenv.config() ;
 import { createClient } from 'redis';
 const client = createClient({
     username: 'default',
-    password: 'SRtGUYyrHtRdH6BHjsbVE1XaW3BP7frB',
+    password: process.env.REDIS_PASSWORD,
     socket: {
         host: 'redis-18620.c114.us-east-1-4.ec2.redns.redis-cloud.com',
         port: 18620
